c650605244280b3dc20cbc27d4e5553d9b830a1b
Mizar Contasti <contasti.mizar@gmail.com>
2021-11-30 21:27:32 -0600

    changes done

--- src/App.js
+++ src/App.js
@@ -1,92 +1,43 @@
-import { Component } from "react";
+import { useFormik } from "formik";
 
-class Input extends Component {
-  //   state = { valor: "" };
-  //   handleChange = (value) => {
-  //     this.setState({ valor: value });
-  //   };
+function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      lastname: "",
+      email: "",
+    },
+    onSubmit: (values) => console.log(values),
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label>Nombre</label>
+      <input
+        name="name"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
 
-  render() {
-    return <input value={this.props.value} onChange={this.props.onChange} />;
-  }
+      <label>Apellido</label>
+      <input
+        name="lastname"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastname}
+      />
+
+      <label>Correo</label>
+      <input
+        name="email"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+
+      <button type="submit">Enviar</button>
+    </form>
+  );
 }
-
-class App extends Component {
-  state = {
-    nombre: "",
-    apellido: "",
-  };
-
-  updateNombre(v) {
-    this.updateValues("nombre", v.target.value);
-  }
-
-  //   updateValues = (prop, value) => {
-  //     this.setState({ [prop]: value });
-  //   };
-  updateValues(prop, value) {
-    this.setState({ [prop]: value });
-  }
-
-  render() {
-    return (
-      <p>
-        Nombre Completo : {`${this.state.nombre}   ${this.state.apellido}`}
-        <Input
-          value={this.state.nombre}
-          onChange={(e) => this.updateValues("nombre", e.target.value)}
-        />
-        <Input
-          value={this.state.apellido}
-          onChange={
-            //   (e) => this.updateValues("apellido", e.target.value)
-            this.updateNombre()
-          }
-        />
-      </p>
-    );
-  }
-}
-
-// class Button extends Component {
-//   state = {};
-//   constructor(props) {
-//     super(props);
-//     console.log("constructor", props);
-//   }
-
-//   componentDidMount() {
-//     console.log("compoenentDidMount");
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     console.log("component did update", prevProps, prevState);
-//   }
-
-//   componentWillUnmount() {
-//     console.log("desmontando componente", this.props, this.state);
-//   }
-
-//   render() {
-//     console.log("ejecutando metodo render de button");
-//     return <button onClick={() => this.setState({ prop: 1 })}>Enviar</button>;
-//   }
-// }
-
-// class App extends Component {
-//   state = { valor: 3 };
-//   render() {
-//     console.log(this.state);
-//     return (
-//       <div>
-//         <p>Hola mundo</p>
-//         {this.state.valor === 3 ? <Button chanchito="feliz" /> : null}
-//         <button onClick={() => this.setState({ valor: 2 })}>
-//           Enviar en App
-//         </button>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
